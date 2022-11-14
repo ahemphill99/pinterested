6e25982182b60d9b577b1688328c52a517a6b8ab
Andrew Hemphill <ahemphill99@gmail.com>
2022-11-14 14:05:12 +1300

    updated login components

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,16 @@ Rails.application.routes.draw do
   #get 'home/index'
   root 'home#index'
   get 'home/about'
+
+
+  
+
+  devise_scope :user do
+get '/users/sign_out' => 'devise/sessions#destroy'
+end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 end
+
