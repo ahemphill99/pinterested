3565cf25ca643d2237c17d097d822880b87e4e2e
Andrew Hemphill <ahemphill99@gmail.com>
2022-11-15 11:24:29 +1300

    added asscns

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_14_011359) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_14_210633) do
   create_table "pins", force: :cascade do |t|
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_pins_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
